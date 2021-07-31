926cd4a55858ac2f7afffbc90e4d72c7f9a18a11
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-31 20:50:03 +0300

    edit style happy-client-section style to pp

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,12 +1,12 @@
 // Add your custom JS here.
 
 
-  $('.img-butt-item').on('click', function(){
+  $('.happy-client-section__image-button-item').on('click', function(){
     let dataImg = $(this).data('img');
     let dataContent = $(`.happy-client-section__text-block[data-content='${dataImg}']`);
     $('.happy-client-section__text-block').removeClass('active')
     dataContent.addClass('active');
-    $('.img-butt-item').removeClass('active')
+    $('.happy-client-section__image-button-item').removeClass('active')
     $(this).addClass('active');
   })
   
